deec54790376ee7f428e03eb188816280a507bb1
LeviCanales <milevi271@gmail.com>
2017-06-24 18:50:31 -0600

    tablero sin imagen de usuario

--- ajax/procesar-tablero.php
+++ ajax/procesar-tablero.php
@@ -3,7 +3,7 @@
  	case '1':
  		for ($i=0; $i < 20; $i++) { 
 		      ?>
-		      <div class="grid-item"><img src="img/icon.png" width="250" height="250" class="img-responsive"><strong>Pinterest Logo</strong><br>Leví Canales<button class="btn btn-danger btn-block">Seguir</button></div>
+		      <div class="grid-item"><img src="img/tablero/<?php echo (($i+1)%15)?>.jpg" width="250px" height="125px"><br><img src="img/tablero/<?php echo (($i+2)%15)?>.jpg" width="125px" height="125px"><img src="img/tablero/<?php echo (($i+3)%15)?>.jpg" width="125px" height="125px"><br><strong>Pinterest Logo</strong><br>Leví Canales<button class="btn btn-danger btn-block">Seguir</button></div>
 		      <?php
 		    }
 		break;
