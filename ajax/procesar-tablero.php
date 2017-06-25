b583ced4a1ee4c89b85c5351ca7f0d210162f80e
LeviCanales <milevi271@gmail.com>
2017-06-24 19:33:41 -0600

    mejor definido el tablero

--- ajax/procesar-tablero.php
+++ ajax/procesar-tablero.php
@@ -3,7 +3,18 @@
  	case '1':
  		for ($i=0; $i < 20; $i++) { 
 		      ?>
-		      <div class="grid-item"><img src="img/tablero/<?php echo (($i+1)%15)?>.jpg" width="250px" height="125px"><br><img src="img/tablero/<?php echo (($i+2)%15)?>.jpg" width="125px" height="125px"><img src="img/tablero/<?php echo (($i+3)%15)?>.jpg" width="125px" height="125px"><br><strong>Pinterest Logo</strong><br>Leví Canales<button class="btn btn-danger btn-block">Seguir</button></div>
+		      <div class="grid-item" style="padding-top: 50px">
+		      	<img src="img/tablero/<?php echo (($i+1)%15)?>.jpg" width="250px" height="125px" class="img-rounded">
+		      	<br>
+		      	<img src="img/tablero/<?php echo (($i+3)%15)?>.jpg" width="125px" height="125px" class="img-rounded">
+		      	<div style="position: absolute; top:185px; left:13px;">
+		      		<img src="img/tablero/<?php echo (($i+4)%15)?>.jpg" width="100px" height="100px" class="img-circle">
+		      	</div>
+		      	<img src="img/tablero/<?php echo (($i+6)%15)?>.jpg" width="125px" height="125px" class="img-rounded">
+		      	<br>
+		      	<strong>Pinterest Logo</strong>
+		      	<br>Leví Canales<button class="btn btn-danger btn-block">Seguir</button>
+		      </div>
 		      <?php
 		    }
 		break;
