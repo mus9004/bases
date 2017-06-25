3aa99218353dd9a6cefef57153b5fc552384ad7f
mus9004 <mus9004@yahoo.es>
2017-06-25 17:34:25 -0600

    MOD

--- perfil.php
+++ perfil.php
@@ -40,55 +40,56 @@
     }
 </style>
 </head>
-<body><nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#"><img src="img/icon.png" width="25" height="25"></a>
-    </div>
-      <form style="margin-top: 8px;">
-          <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search">
-            <div class="input-group-btn">
-                <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                  Tableros <span class="glyphicon glyphicon-chevron-down"></span>
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                  <li><a href="#">Todos los Pines</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">Tus Pines</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">Pines que se pueden comprar</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">Gente</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">Tableros <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></a></li>
-                </ul>
-                <button type="button" class="btn btn-default dropdown-toggle" id="2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categorías <span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
-                </button>
-                <button type="button" class="btn btn-default">Guardado
-                  <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-                </button>
-                <button type="button" class="btn btn-default">Notificaciones
-                  <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
-                </button>
-            </div>
+<body>
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#"><img src="img/icon.png" width="25" height="25"></a>
           </div>
-      </form>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+            <form style="margin-top: 8px;">
+                <div class="input-group">
+                  <input type="text" class="form-control" placeholder="Search">
+                  <div class="input-group-btn">
+                      <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        Tableros <span class="glyphicon glyphicon-chevron-down"></span>
+                      </button>
+                      <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                        <li><a href="#">Todos los Pines</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Tus Pines</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Pines que se pueden comprar</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Gente</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Tableros <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></a></li>
+                      </ul>
+                      <button type="button" class="btn btn-default dropdown-toggle" id="2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categorías <span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
+                      </button>
+                      <button type="button" class="btn btn-default">Guardado
+                        <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                      </button>
+                      <button type="button" class="btn btn-default">Notificaciones
+                        <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
+                      </button>
+                  </div>
+                </div>
+            </form>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
 
-    <div row>
+    <div >
       <br>
       <div class="col-sm-12">
           <div class="col-sm-8 col-sm-offset-2" >
-            <!--<div id="btns-subnav">
+            <div id="btns-subnav">
               <a style="margin-left:15px;" class="idnav" href="#"><i class="glyphicon glyphicon-cog"></i></a>
               <a class="idnav" href="#"><i class="glyphicon glyphicon-share-alt"></i></a>
               <a class="idnav" href="#"><i class="glyphicon glyphicon-option-horizontal"></i></a>
               <br>
-              </div>-->
+              </div>
             <div class="col-sm-6">
               <h1 id="nombrePerfil">Elisa Gabriela Zelaya</h1>
               </div>
@@ -111,24 +112,31 @@
               </center>
              </div>
           </div>
-          <div clas="col-sm-12">
+
+          <div class="col-sm-12">
             <div class="col-sm-8 col-sm-offset-2">
               <ul class="nav nav-pills">
               <li role="presentation" class="active"><a href="#">Tableros</a></li>
               <li role="presentation"><a href="#">Pines</a></li>
                </ul>
               <br>
-            <div class="well col-sm-3">
-              <center><a href="#"><i  style="font-size:35px; margin-top: 35px"class="glyphicon glyphicon-plus-sign"></i></a></center></div>
-             
-              </div> 
+              <div class="well col-sm-3">
+                <center><a href="#"><i  style="font-size:35px; margin-top: 35px"class="glyphicon glyphicon-plus-sign"></i></a></center>
+              </div>
+            </div> 
+
             <div class="col-sm-2 col-sm-offset-2">
                 <p id="etiquetacreartablero"><b>Crear tablero</b></p>
-              </div>
+                <div></div>
+                <div class="well" clas="col-sm-12" style="width: 100%"></div>
+            </div>
+
           </div>
+         
       </div>
 
     </div>
+    
     
 </body>
 </html>
