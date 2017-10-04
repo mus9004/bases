3491176f1a9d547eb20520edc4b6b97124aeb6e0
LeviCanales <milevi271@gmail.com>
2017-10-04 14:02:52 -0600

    conexion exitosa

--- class/class-conexion-oracle.php
+++ class/class-conexion-oracle.php
@@ -15,7 +15,7 @@
         public function __construct() {
  
             $this->Servidor = '127.0.0.1';
-            $this->NombreBD = 'pinterestdb';
+            $this->NombreBD = 'DB_PINTEREST';
             $this->Usuario = 'SYSTEM';
             $this->Clave = 'oracle';
             $this->Puerto = '1521'; // PUERTO QUE ESCUCHA ORACLE
@@ -41,14 +41,11 @@
             $intentos = 0;
             while (!is_resource($this->LinkId) && $intentos < 20) {
                 $intentos++;
-                $this->LinkId = oci_connect($this->Usuario, $this->Clave, "(DESCRIPTION = (LOAD_BALANCE = yes)
-                               (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP) (HOST = {$this->Servidor}) (PORT = 1521) ) )
-                               (CONNECT_DATA = (FAILOVER_MODE = (TYPE = select) (METHOD = basic) (RETRIES = 180) (DELAY = 5) )
-                               (SERVICE_NAME = {$this->NombreBD}) ) )");
+                $this->LinkId = oci_connect($this->Usuario, $this->Clave, 'localhost/XE');
             }
  
             if (is_resource($this->LinkId)) {
-                self::$sMensaje = "Conexion exitosa!<br/>";
+                self::$Mensaje = "Conexion exitosa!<br/>";
             } else {
                 self::$Mensaje = "ERROR: No se puede conectar a la base de datos..!<br/>";
             }
