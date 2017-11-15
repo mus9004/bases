53dbf3cb49b8808f716549641bdfde235e288ec4
LeviCanales <milevi271@gmail.com>
2017-11-15 04:18:50 -0600

    jALA EL NOMBRE Y LA FOTO

--- ajax/acceso.php
+++ ajax/acceso.php
@@ -10,8 +10,13 @@ switch ($_GET['accion']) {
 	case '2':
 		$conexion = new Conexion();
   		$conexion->conectar();
-		$datos= array();
-		echo json_encode($datos);
+  		$consulta=$conexion->ejecutarInstruccion("select A.CODIGO_IDIOMA, A.CODIGO_GENERO, A.ALIAS, A.NOMBRE, B.APELLIDO, TO_CHAR(B.FECHA_NACIMIENTO, 'DD/MM/YYYY') as FECHA_NACIMIENTO, C.URL_FOTO
+			from TBL_USUARIO A, TBL_PERSONA B, TBL_FOTOS C
+			WHERE A.CODIGO_USUARIO = B.CODIGO_PERSONA(+)
+			AND A.CODIGO_FOTO = C.CODIGO_FOTO(+)
+			and A.CODIGO_USUARIO=".$_SESSION['codigo_usuario']);
+		$linea=$conexion->obtenerRegistro($consulta);
+		echo json_encode($linea);
 		$conexion->desconectar();
 		break;
 	default:
